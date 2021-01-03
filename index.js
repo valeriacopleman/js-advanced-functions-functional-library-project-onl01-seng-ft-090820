53ff6a4fe9d4dce16107e63c3e96ae982e1252e4
Valeria Copleman <valeriacopleman@gmail.com>
2021-01-03 14:06:33 -0800

    commit tree 18a843dbedc9d6d6c8b1a41bd12259f3089eff79

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(arr) {
+      return arr
     },
 
     map: function() {
